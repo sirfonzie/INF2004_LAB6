f93e299e365dfb4297c74c01f91d17084fee64d9
sirfonzie <33278390+sirfonzie@users.noreply.github.com>
2023-10-02 12:04:33 +0800

    Add files via upload

---
+++ pid.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float Kp = 2.0; 
+float Ki = 0.2; 
+float Kd = 0.02; 
+
+// Function to compute the control signal
+float compute_pid(float setpoint, float current_value, float *integral, float *prev_error) {
+
+    float error = current_value - setpoint;
+    
+    *integral += error;
+    
+    float derivative = error - *prev_error;
+    
+    float control_signal = Kp * error + Ki * (*integral) + Kd * derivative * 0.1;
+    
+    *prev_error = current_value;
+    
+    return control_signal;
+}
+
+int main() {
+    float setpoint = 100.0;  // Desired position
+    float current_value = 0.0;  // Current position
+    float integral = 0.0;  // Integral term
+    float prev_error = 0.0;  // Previous error term
+    
+    float time_step = 0.1;  
+    int num_iterations = 100 
+    
+    // Simulate the control loop
+    for (int i = 0; i < num_iterations; i++) {
+        float control_signal = compute_pid(setpoint, current_value, &integral, prev_error);
+        
+        float motor_response = control_signal * 0.05;  // Motor response model
+        
+        printf("Iteration %d: Control Signal = %d, Current Position = %f\n", i, control_signal, current_value);
+        
+        prev_error = current_value;
+        
+        usleep((useconds_t)(time_step));
+    }
+    
+    return 0;
+}
